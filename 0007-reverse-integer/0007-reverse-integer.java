dc2e934dc51560a7d9a9d41e17532032a512b87f
Akhilesh Dhakad <akhidhakar623@gmail.com>
2026-08-26 20:52:31 +0530

    Time: 1 ms (99.98%), Space: 42.8 MB (11.31%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int reverse(int x) {
+         int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            // 32-bit overflow check
+            if (rev > 214748364 ||
+                (rev == 214748364 && digit > 7)) {
+                return 0;
+            }
+
+            if (rev < -214748364 ||
+                (rev == -214748364 && digit < -8)) {
+                return 0;
+            }
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
+    }
+}
